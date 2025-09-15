df79166e85d95317df518207d2f7104868fa6958
DanielPDamiao <daniel.pereira.damiao@ccc.ufcg.edu.br>
2025-09-15 00:35:14 -0300

    Revert "Possivel rodar o aplicativo de qualquer local da maquina utilizando o caminho ate o arquivo principal. Ex: 'swipl Usuario/Documents/Programas/analisador-sintatico/app/analisador.pl'"
    This reverts commit e6d1f899d3417ad0028a6441a293cc7eea31ed4b.

--- app/setup.pl
+++ app/setup.pl
@@ -6,7 +6,8 @@
 % astSaveResultsDir: diretório onde os resultados da análise serão salvos. (CRIADO CASO NÃO EXISTA)
 % Para modificar os diretórios mude o segundo parâmetro da função para um caminho terminado em '/' tendo como referencia o diretorio atual deste arquivo.
 set_base_dirs :-
-    source_file(set_base_dirs, File),
+    write('rodou aqui'),
+    source_file(set_base_dirs, File), write(File),
     file_directory_name(File, Dir),
     working_directory(_, Dir).
 
