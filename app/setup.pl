b977adeecc4bd4a3f10dd52df5595f5595a27fcf
DanielPDamiao <daniel.pereira.damiao@ccc.ufcg.edu.br>
2025-09-15 13:32:44 -0300

    Mudança no diretorio base para comandos e configurações.

--- app/setup.pl
+++ app/setup.pl
@@ -4,16 +4,17 @@
 % Setup inicial do programa, definindo diretórios, etc.
 % localArquivesDir: diretório de leitura dos arquivos de teste local. (NECESSÁRIO EXISTENCIA PREVIA)
 % astSaveResultsDir: diretório onde os resultados da análise serão salvos. (CRIADO CASO NÃO EXISTA)
-% Para modificar os diretórios mude o segundo parâmetro da função para um caminho terminado em '/' tendo como referencia o diretorio atual deste arquivo.
+% Para modificar os diretórios mude o segundo parâmetro da função para um caminho terminado em '/' tendo como referencia o diretorio 'analisador-sintatico'.
 set_base_dirs :-
     source_file(set_base_dirs, File),
     file_directory_name(File, Dir),
-    working_directory(_, Dir).
+    atom_concat(BaseDir, 'app', Dir),
+    working_directory(_, BaseDir).
 
 configs :-
-    nb_setval(localArquivesDir, '../tests/Examples/'),
-    nb_setval(astSaveResultsDir, '../ast_results/'),
-    verifica_dirs.
+    nb_setval(localArquivesDir, './tests/Examples/'),
+    nb_setval(astSaveResultsDir, './ast_results/'),
+    verifica_dirs, verifica_compatibilidade.
 
 verifica_dirs :-
     nb_getval(localArquivesDir, Dir1),
@@ -25,3 +26,10 @@ verifica_dirs :-
     (Ultimo1 == '/', Ultimo2 == '/', exists_directory(Dir1) -> 
         true; 
     write('Configuração de diretorios invalida. Revise as configurações em "configs" no programa "setup" e tente novamente.\n'), halt).
+
+verifica_compatibilidade :-
+    ((current_prolog_flag(readLine, _); 
+    current_prolog_flag(editline, _); 
+    current_prolog_flag(tty_control,_)) -> 
+        write('\n(Possivel compatibilidade com funções de linha.)');
+    write('\n(Provavel falta de compatibilidade com funções de linha)')).
